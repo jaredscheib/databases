0f9c9224ad7ee1c4f67e169909bf623a28ca1655
Hack Reactor Students <jared.scheib@gmail.com>
2014-06-27 21:44:32 -0700

    Implement persistent-server

--- SQL/persistent-server.js
+++ SQL/persistent-server.js
@@ -1,1 +1,16 @@
+//require http helper
+var http = require('http');
+//require requestHandler
+var requestHandler = ('requestHandler');
 
+//define IP
+var ip = '127.0.0.1';
+
+//define port
+var port = 8080;
+
+//create server
+var server = http.createServer(requestHandler);
+
+//server listen
+server.listen(ip, port);
